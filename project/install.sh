5158f2d5ea216e5e882af62e4f0ee0daf1984f5f
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-22 21:01:10 +0200

    Update install.sh

--- project/install.sh
+++ project/install.sh
@@ -2,3 +2,4 @@
 
 echo "🏎️ Please make sure SuperTuxKart is not installed on your computer."
 sudo ln -sf "/usr/bitey/software/supertuxkart/supertuxkart" "/usr/bin/supertuxkart"
+sudo chmod +x "/usr/bin/supertuxkart"
